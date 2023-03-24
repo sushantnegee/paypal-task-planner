0b636113531b2aeadc34115329842188ae02f981
sushantnegee <sushantnegee@gmail.com>
2023-03-24 14:26:43 +0530

    connection established with atlas

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require('express');
+const connectDataBase = require('./config/db');
+const dotenv = require('dotenv')
 
 const app = express();
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
+connectDataBase();
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })

b4cc8a423dbc5456744d44018a23c3ec06a2a537
sushantnegee <sushantnegee@gmail.com>
2023-03-24 14:58:27 +0530

    schema created

---
+++ server/models/sprintModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const sprintSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+},{
+    timestamps:true,
+});
+
+const Sprint = mongoose.model('Sprint', sprintSchema);
+
+module.exports = Sprint;
---
+++ server/models/taskModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  type: { type: String, enum: ['bug', 'feature', 'story'], required: true },
+  assignee: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  status: { type: String, enum: ['todo', 'in-progress', 'done'], default: 'todo' },
+},{
+    timestamps:true,
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;

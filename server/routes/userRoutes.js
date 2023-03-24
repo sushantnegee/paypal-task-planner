cf2798a8aed068a7a7873dbbc1fcfe38585a2abe
sushantnegee <sushantnegee@gmail.com>
2023-03-24 19:15:04 +0530

    login route created

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -26,7 +26,7 @@ router.post("/register", async (req, res) => {
     if (user) {
       res.status(201).json({
         _id: user._id,
-        name: (await user).name,
+        name: user.name,
         email: user.email,
         token: generateToken(user._id), // using jwt and method is in config/generateToken.js
       });
@@ -38,6 +38,33 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+
+router.post('/login',async (req,res)=>{
+  try {
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email})
+    if(!user){
+      res.status(401);
+      throw new Error('Invalid Id or Password')
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(!validPassword){
+      return res.status(400).json({message: 'Invalid credentials'})
+    }
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id), // using jwt and method is in config/generateToken.js
+    });
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+})
 
 
 module.exports = router;

8dd361603aff43f8ef8a1cfb98abfdb06caab059
sushantnegee <sushantnegee@gmail.com>
2023-03-25 11:52:50 +0530

    context update with sprint

--- client/src/Context/ContextProvider.jsx
+++ client/src/Context/ContextProvider.jsx
@@ -5,7 +5,19 @@ export const AppContext = createContext();
 const ContextProvider = ({children}) => {
   const [user, setUser] = useState("");
   const [loggedIn,setLoggedIn] = useState(false);
-  const [sprints,setSprints] = useState('');
+  const [sprints,setSprints] = useState([{
+    "name": "Sprint 1",
+    "startDate": "2023-04-01",
+    "endDate": "2023-03-05"
+  },{
+    "name": "Sprint 2",
+    "startDate": "2023-04-01",
+    "endDate": ""
+  },{
+    "name": "Sprint 3",
+    "startDate": "2023-04-01",
+    "endDate": "2023-03-05"
+  }]);
 
   useEffect(()=>{
       const userInfo = JSON.parse(localStorage.getItem("userInfo"));
